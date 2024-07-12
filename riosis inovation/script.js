a1125ec6cfc1fc2ba9cd52a7fe512134587f0524
Brtkrish <s.bharathkrishna001@gamil.com>
2024-07-12 12:56:02 +0530

    added contact us, images,slider

---
+++ riosis inovation/script.js
@@ -0,0 +1,17 @@
+let currentSlide = 0;
+const slider = document.getElementById('slider');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+function changeSlide(direction) {
+    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+    slider.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+function autoSlide() {
+    changeSlide(1);
+}
+
+setInterval(autoSlide, 3000);
+
+document.querySelector('.prev').addEventListener('click', () => changeSlide(-1));
+document.querySelector('.next').addEventListener('click', () => changeSlide(1));
